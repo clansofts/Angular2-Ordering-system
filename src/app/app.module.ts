3a64d04cedad56f9975e7dc3f1d04ac529f857b6
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-04-04 18:56:52 +0200

    Add Login&Signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AuthenticationService } from './_services/authentication.service';
+import { UserService } from './_services/user.service';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: '**', redirectTo: '' }
+    ])
   ],
-  providers: [],
+  providers: [
+    AuthenticationService,
+    UserService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
