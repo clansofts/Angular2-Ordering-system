239b199b1e9f85b0b495900cbab6ae140ab4f200
seifElislam <seif.eleslam1990@gmail.com>
2017-04-06 22:55:35 +0200

    groups almost finish but has bug

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -3,6 +3,10 @@ import { GroupsService } from '../_services/groups.service';
 import { Group } from '../_models/Group';
 import { User } from '../_models/user';
 import { AuthenticationService } from '../_services/authentication.service';
+import {UserService} from '../_services/user.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-groups',
@@ -14,12 +18,34 @@ export class GroupsComponent implements OnInit {
 
   currentUser: User;
   groups: Group[];
-  currentGroup;
+  currentGroup:Group;
+  invited_users = [];
+  tmp: any = {invite: {}, invite_valid: true};
+  loading = false;
+  searching = false;
+  searchFailed = false;
 
-  constructor(private groupService: GroupsService,private uerService: AuthenticationService) {
+  constructor(private groupService: GroupsService,private uerService: AuthenticationService,private _user: UserService) {
       this.currentUser=uerService.getCurrentUser();
       console.log("user",this.currentUser);
   }
+
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .do(() => this.searching = true)
+      .switchMap(term =>
+        this._user.search({field: "name", q: term})
+          .do(() => this.searchFailed = false)
+          .catch(() => {
+            this.searchFailed = true;
+            return Observable.of([]);
+          }))
+      .do(() => this.searching = false);
+
+  formatter = (x: {name: string}) => x.name;
+
 
   ngOnInit() {
 
@@ -41,5 +67,35 @@ export class GroupsComponent implements OnInit {
       data =>{},error => {}
     );
   }
+
+  onSelect(group: Group): void {
+  this.currentGroup = group;
+  console.log(this.currentGroup);
+}
+inviteItemTmp(event) {
+  this.tmp.invite = event.item;
+}
+
+inviteItemHandle() {
+  var exists = false;
+  for (var i = 0; i < this.invited_users.length; i++) {
+    if (this.invited_users[i]._id == this.tmp.invite._id) {
+      exists = true;
+    }
+  }
+  if (exists) {
+    this.tmp.invite_valid = false;
+  } else {
+    this.invited_users.push(this.tmp.invite);
+  }
+}
+
+removeUser(user){
+  for (var i = 0; i < this.invited_users.length; i++) {
+    if (this.invited_users[i]._id == user._id) {
+      this.invited_users.splice(i, 1);
+    }
+  }
+}
 
 }
