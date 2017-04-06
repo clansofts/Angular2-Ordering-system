9fe9370af44412b2b015cae80c0de3835b8600b4
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-04-06 14:57:17 +0200

    Add Functions to 'Add Order' Page

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,50 +1,75 @@
-﻿import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
+﻿import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions, Response} from '@angular/http';
 
-import { User } from '../_models/user';
+import {URLSearchParams, Jsonp} from '@angular/http';
+
+
+import {User} from '../_models/user';
 import {Observable} from "rxjs";
 
 @Injectable()
 export class UserService {
-    constructor(private http: Http) { }
+  constructor(private http: Http, private _jsonp: Jsonp) {
+  }
 
-    getAll() {
-        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+  getAll() {
+    return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+  }
+
+  getById(id: number) {
+    return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+  }
+
+  create(user: User) {
+    return this.http.post('http://localhost:8090/auth/register', user, this.jwt()).map((response: Response) => response.json());
+  }
+
+  update(user: User) {
+    return this.http.put('/api/users/' + user._id, user, this.jwt()).map((response: Response) => response.json());
+  }
+
+  delete(id: number) {
+    return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+  }
+
+  search(term: string) {
+    if (term === '') {
+      return Observable.of([]);
     }
 
-    getById(id: number) {
-        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
-    }
+    let searchUrl = 'http://localhost:8090/users/search';
 
-    create(user: User) {
-        return this.http.post('http://localhost:8090/auth/register', user, this.jwt()).map((response: Response) => response.json());
-    }
+    let params = new URLSearchParams();
+    params.set('field', 'name');
+    params.set('q', term);
 
-    update(user: User) {
-        return this.http.put('/api/users/' + user._id, user, this.jwt()).map((response: Response) => response.json());
-    }
+    let options = this.jwt();
+    options.search = params;
 
-    delete(id: number) {
-        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
-    }
-
-    getByEmail(term: string){
     return this.http
-      .get('http://localhost:8090/users/'+term,this.jwt())
+      .get(searchUrl, options)
+      .map(response => <string[]> response.json());
+
+  }
+
+
+  getByEmail(term: string) {
+    return this.http
+      .get('http://localhost:8090/users/' + term, this.jwt())
       .map(response => response.json());
-    }
+  }
 
-    makeFriendShip(id: string){
-      return this.http
-        .get('http://localhost:8090/follow/'+id,this.jwt())
-        .map(response => response.json());
-    }
+  makeFriendShip(id: string) {
+    return this.http
+      .get('http://localhost:8090/follow/' + id, this.jwt())
+      .map(response => response.json());
+  }
 
-    private jwt() {
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-            return new RequestOptions({ headers: headers });
-        }
+  private jwt() {
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.token) {
+      let headers = new Headers({'Authorization': 'Bearer ' + currentUser.token});
+      return new RequestOptions({headers: headers});
     }
+  }
 }
--- src/app/add-order/add-order.component.ts
+++ src/app/add-order/add-order.component.ts
@@ -1,30 +1,62 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 
-import { AlertService } from '../_services/alert.service';
-import { AuthenticationService } from '../_services/authentication.service';
+import {AlertService} from '../_services/alert.service';
+import {AuthenticationService} from '../_services/authentication.service';
+import {UserService} from '../_services/user.service';
+
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 @Component({
   templateUrl: './add-order.component.html',
   styleUrls: ['./add-order.component.css']
 })
+
 export class AddOrderComponent implements OnInit {
   model: any = {};
+
   loading = false;
+  searching = false;
+  searchFailed = false;
 
-  order_for_values = ["Lunch","BreackFast"];
+  tmp: any = {invite : {}};
+  invited_users = [];
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private alertService: AlertService
-  ) { }
+  order_for_values = ["Lunch", "BreackFast"];
+
+  constructor(private _user : UserService) {
+  }
+
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .do(() => this.searching = true)
+      .switchMap(term =>
+        this._user.search(term)
+          .do(() => this.searchFailed = false)
+          .catch(() => {
+            this.searchFailed = true;
+            return Observable.of([]);
+          }))
+      .do(() => this.searching = false);
+
+  formatter = (x: {name: string}) => x.name;
+
+  inviteItemTmp(event){
+    this.tmp.invite = event.item;
+  }
+
+  inviteItemHandle(){
+    this.invited_users.push(this.tmp.invite);
+  }
 
   ngOnInit() {
   }
 
-  addOrder(){
+  addOrder() {
 
   }
 
