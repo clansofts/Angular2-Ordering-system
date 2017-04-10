0e2b9962a8ca3699c23ef01b8480dd41768c4357
seifElislam <seif.eleslam1990@gmail.com>
2017-04-10 21:55:48 +0200

    divide get friends orders into 2 steps

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import {UtilService} from "../_services/util.service";
 import { Order } from '../_models/order';
 import {UserService} from '../_services/user.service';
 import { AuthenticationService } from '../_services/authentication.service';
+import {FriendsService} from "../_services/friends.service";
 
 @Component({
   selector: 'app-home',
@@ -17,18 +18,29 @@ export class HomeComponent implements OnInit {
   currentUser: User;
   friends:User[]=[];
   myOrders:Order[];
-  friendsOrders:Order[]=[];
+  friendsOrders:Order[];
 
-  constructor(private homeService: HomeService,private uerService: AuthenticationService) {
+  constructor(private homeService: HomeService,private uerService: AuthenticationService,private friendService: FriendsService) {
     this.currentUser=uerService.getCurrentUser();
    }
 
   ngOnInit() {
-     console.log(this.homeService.getMyOrders(this.currentUser._id));
+     //console.log(this.homeService.getMyOrders(this.currentUser._id));
+     //get my orders
     this.homeService.getMyOrders(this.currentUser._id).then((orders)=>{console.log("result :",orders);
                                                                         this.myOrders=orders;
                                                                         console.log("my orders:",this.myOrders);
                                                                         });
+    //get My friends
+    this.friendService.getFriends(this.currentUser._id).then((friends)=>{console.log("friends result :",friends);
+                                                                        this.friends=friends;
+                                                                        console.log("my friends:",this.friends);
+                                                                        });
+    //get my friends's orders
+    // this.homeService.getFriendsOrders(this.currentUser._id).then((orders)=>{console.log("friends result :",orders);
+    //                                                                     this.friendsOrders=orders;
+    //                                                                     console.log("my friends:",this.friendsOrders);
+    //                                                                     });
 
   }
 
