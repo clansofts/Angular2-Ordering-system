044e9273ebbefeffb61a8bc56192150273b42cfa
muhammad-habib <muhammad.habib.dev@gmail.com>
2017-04-15 00:17:02 +0200

    fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,20 @@
-import {Component, OnChanges, OnInit} from '@angular/core';
-import {AuthenticationService} from './_services/authentication.service';
-import {Location} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "./_services/authentication.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit{
   title = 'app works!';
 
   constructor(
-      private location: Location,
       private _auth: AuthenticationService,
     ) {}
 
 
   ngOnInit() {
-    console.log(this.location.path());
   }
-
-  ngOnChanges(){
-    console.log(this.location.path());
-  }
-
-
 
 
 
