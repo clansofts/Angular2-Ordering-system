326d6c68a9d142ad8cd8369b32a58b814d92df94
muhammad-habib <muhammad.habib.dev@gmail.com>
2017-04-17 17:28:06 +0200

    fix bug

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class AppSettings {
-  public static API_ENDPOINT = 'https://iti-yala-notlop.herokuapp.com';
-  //public static API_ENDPOINT = 'http://localhost:8090';
+  //public static API_ENDPOINT = 'https://iti-yala-notlop.herokuapp.com';
+  public static API_ENDPOINT = 'http://localhost:8080';
 
 }
