4126e434055a2e6aafa9e9d7128afefb4e25a14d
muhammad-habib <muhammad.habib.dev@gmail.com>
2017-04-17 18:18:20 +0200

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -2478,7 +2478,7 @@ exports = module.exports = __webpack_require__(19)();
 
 
 // module
-exports.push([module.i, "body {\n\n}\n\n.navbar {\n  margin-top: 20px;\n  margin-bottom: 20px;\n}\n\n\n.avatar{\n  background: url(\"/https:/github.com/muhammad-habib/Angular2-Ordering-system/src/assets/images/default-avatar.jpg\");\n}\n\n\n.navbar-avatar {\n  width: 32px;\n  height: 32px;\n  margin-bottom: -.5rem;\n  margin-top: -.5rem;\n}\n", ""]);
+exports.push([module.i, "body {\n\n}\n\n.navbar {\n  margin-top: 20px;\n  margin-bottom: 20px;\n}\n\n\n.avatar{\n  background: url(\"/https:/muhammad-habib.github.io/Angular2-Ordering-system/src/assets/images/default-avatar.jpg\");\n}\n\n\n.navbar-avatar {\n  width: 32px;\n  height: 32px;\n  margin-bottom: -.5rem;\n  margin-top: -.5rem;\n}\n", ""]);
 
 // exports
 
